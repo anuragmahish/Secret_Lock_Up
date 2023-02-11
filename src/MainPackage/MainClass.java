bc5fa1a6ceac484cd79a781c2da8158275a1d9f2
anuragmahish <anuragmahish.1801@gmail.com>
2023-02-11 12:12:57 +0530

    Start project with token authentications

--- src/MainPackage/MainClass.java
+++ src/MainPackage/MainClass.java
@@ -3,6 +3,6 @@ package MainPackage;
 public class MainClass {
 	public static void main(String args[])
 	{
-		System.out.println("working on git");
+		System.out.println("working on git with tokens");
 	}
 }
